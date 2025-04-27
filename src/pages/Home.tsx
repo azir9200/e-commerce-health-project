649c0bac8ea8bbcfb523bcbf5b468bd16753ed1e
Azir@9200 <aziruddin83@hotmail.com>
2025-04-28 00:24:21 +0100

    navbar fixeds

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,7 @@
-// import ProductCard from "./ProductCard";
-import ProductCart from "../components/ProductCard";
 import Products from "../components/Products";
 import HomeSlide from "../components/shareHome/Carousal";
-import ProductCardSkeleton from "../components/Skeleton/ProductCartSkeleton";
-import { useGetAllProductQuery } from "../redux/api/productApi/ProductApi";
 
 const Home = () => {
-  const { data, isLoading } = useGetAllProductQuery(null);
-  const products = data?.data;
   return (
     <div className=" border border-red-600">
       <HomeSlide />
@@ -15,18 +9,7 @@ const Home = () => {
       <Products />
       <div className="bg-green-600 m-4">
         <div className=" bg-red-400 h-20 ">
-          <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
-            {isLoading
-              ? Array.from({ length: 8 }).map((_, index) => (
-                  <ProductCardSkeleton key={index} />
-                ))
-              : products?.map.slice(
-                  0,
-                  8
-                )((product: any) => (
-                  <ProductCart key={product._id} product={product} />
-                ))}
-          </div>
+          <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4"></div>
         </div>
       </div>
     </div>
